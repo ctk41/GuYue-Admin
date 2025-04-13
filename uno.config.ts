6bc40dee7e3dd8dd0bc2cdd8097d8bc3a0dbc0d2
chinh.duongvan <chinh.duongvan@vti.com.vn>
2025-04-13 17:49:59 +0700

    feat: config uno css

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,7 @@ import presetUno from '@unocss/preset-uno';
 import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
+  warn: false,
   content: {
     pipeline: {
       exclude: ['node_modules', 'dist', '.git', '.husky', '.vscode', 'public', 'build', 'mock', './stats.html'],
